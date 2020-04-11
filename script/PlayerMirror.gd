813007cbb2e59691f80d8d382f7f9b8a7dc06323
treekanick0 <treekanicko@live.com>
2020-04-11 10:56:09 -0600

    Added mirror, control with left/right, fixed collision layers and masks

---
+++ script/PlayerMirror.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export (int) var rotation_speed = 2
+var rotation_direction = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Allow other scripts to update the rotation direction
+func set_rotation_direction(rotation_dir):
+    rotation_direction = rotation_dir
+
+
+# Function to process motion
+func _physics_process(delta):
+    
+    #rotation += rotation_direction * rotation_speed * delta
+    rotate(rotation_direction * rotation_speed * delta)
