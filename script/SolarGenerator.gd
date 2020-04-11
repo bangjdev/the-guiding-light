a95ca93610f3411cef837ca3e52ea9a0a9bf88fb
bangjdev <bangjdev@gmail.com>
2020-04-11 23:56:24 +0200

    Add a basic light source, using raycast2d to detect collision

--- script/SolarGenerator.gd
+++ script/SolarGenerator.gd
@@ -12,17 +12,17 @@ var has_power = false
 
 # Allow children to see if the generator has power
 func is_powered() -> bool:
-    return has_power
+	return has_power
 
 
 # Do kinematics
 # (Actually, just check if there is any light shining on it
 func _physics_process(delta):
-    
-    if Input.is_action_pressed("debug_key"):
-        has_power = true
-        self_light.enabled = true
-    else:
-        has_power = false
-        self_light.enabled = false
-        
+	
+	if Input.is_action_pressed("debug_key"):
+		has_power = true
+		self_light.enabled = true
+	else:
+		has_power = false
+		self_light.enabled = false
+		
