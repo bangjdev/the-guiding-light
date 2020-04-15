92becca32a683c2b3c24b92512e75ad91abc466b
bangjdev <bangjdev@gmail.com>
2020-04-15 22:57:21 +0200

    Add level 1, gather all levels in mainscene

--- script/PlayerMirror.gd
+++ script/PlayerMirror.gd
@@ -6,6 +6,9 @@ var rotation_direction = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func i_am_reflective() -> bool:
+	return true
 
 
 # Allow other scripts to update the rotation direction
