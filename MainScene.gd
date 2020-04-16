28b72c3d1864ef036ae3d4abec3d2ae5aef64bef
bangjdev <bangjdev@gmail.com>
2020-04-16 18:31:12 +0200

    Changed darkness

--- MainScene.gd
+++ MainScene.gd
@@ -8,7 +8,7 @@ onready var music = get_node("AudioStreamPlayer2D")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    music.play()
+	music.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
